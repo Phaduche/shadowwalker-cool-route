3e8e6acefde9ed19fb7637b64e7c2c75fd3870eb
YEAN BAN <byeanb16@gmail.com>
2026-06-10 20:02:44 +0900

    fixed api issue + got new api for security

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,7 +1,12 @@
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY
+
 export async function getWeather(lat, lon) {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=77a81b6ab6e61d00e6ed3f638072179d&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
 
   return await res.json()
 }
+
+console.log(import.meta.env.VITE_OPENWEATHER_API_KEY)
+console.log("API KEY:", API_KEY)
